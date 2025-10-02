119c7a2d3ac2c40b43e93d7c7e4f9e4625c28c05
santiagota <santiagota@T25005>
2025-10-02 13:18:07 +0200

    ..

---
+++ Scheduler_Test/Services/ServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Scheduler_Lib.Classes;
+
+namespace Scheduler_Lib.Services
+{
+    public class ServiceTest
+    {
+        [Fact]
+        public void CalcDate_NullArgument()
+        {
+            RequestedDate testRequested = null;
+            Assert.Throws<Exception>(() => Service.CalcDate(testRequested));
+        }
+
+        /*[Fact]
+        public void CalcDate_Enabled() {
+            RequestedDate testRequested = new RequestedDate();
+            testRequested.Enabled = false;
+            SolvedDate solvedDate = new SolvedDate
+            {
+                NewDate = requestedDate.Date,
+                Description = "Desactivado: No se ha realizado ninguna modificación"
+            };
+            Assert.Equal(Service.CalcDate(testRequested), solvedDate);
+        }*/
+
+    }
+}
