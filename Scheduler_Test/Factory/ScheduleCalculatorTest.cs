4cf5b945d843bb79cc2d3ade3f87e8e0cc9c8b05
santiagota <santiagota@T25005>
2025-10-03 10:17:17 +0200

    .

---
+++ Scheduler_Test/Factory/ScheduleCalculatorTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Scheduler_Lib.Enum;
+using Scheduler_Lib.Interfaz;
+
+namespace Scheduler_Lib.Factory
+{
+    public class ScheduleCalculatorTest
+    {
+        [Fact]
+        public void GetScheduleCalculator_Once()
+        {
+            var calc = ScheduleCalculator.GetScheduleCalculator(Periodicity.OneTime);
+            Assert.IsAssignableFrom<ISchedule>(calc);
+            Assert.Equal("Scheduler_Lib.Services.CalcOneTime", calc.GetType().FullName);
+        }
+
+        [Fact]
+        public void GetScheduleCalculator_Recurrent()
+        {
+            var calc = ScheduleCalculator.GetScheduleCalculator(Periodicity.Recurrent);
+            Assert.IsAssignableFrom<ISchedule>(calc);
+            Assert.Equal("Scheduler_Lib.Services.CalcRecurrent", calc.GetType().FullName);
+        }
+
+        [Fact]
+        public void GetScheduleCalculator_Unsuported()
+        {
+            var calc = (Periodicity)5;
+            var result = Assert.Throws<Exception>(() => ScheduleCalculator.GetScheduleCalculator(calc));
+            Assert.Equal("Unsupported periodicity", result.Message);
+        }
+    }
+}
---
+++ Scheduler_Test/Services/CalcDateTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Scheduler_Lib.Classes;
+
+namespace Scheduler_Lib.Services
+{
+    public class CalcDateTest
+    {
+        public void NullRequest()
+        {
+            RequestedDate requestedDate = null;
+            var result = Assert.Throws<Exception>(() => Service.CalcDate(requestedDate));
+            Assert.Equal("Error: The request shouldn't be null.", result.Message);
+        }
+    }
+}
