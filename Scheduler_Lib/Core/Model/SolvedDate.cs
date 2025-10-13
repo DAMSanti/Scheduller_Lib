b72b89583b781ce7e6d7b28b68a1760ecb552aa7
santiagota <santiagota@T25005>
2025-10-13 11:55:01 +0200

    .

--- Scheduler_Lib/Core/Model/SolvedDate.cs
+++ Scheduler_Lib/Core/Model/SolvedDate.cs
@@ -1,6 +1,6 @@
 ﻿namespace Scheduler_Lib.Core.Model;
 
-public class SolvedDate {
+public struct SolvedDate() {
     public string Description { get; set; } = string.Empty;
     public DateTimeOffset NewDate { get; set; }
     public List<DateTimeOffset>? FutureDates { get; set; }
